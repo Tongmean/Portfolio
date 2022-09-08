f978220f11808902ade4c36aa690ed33fccc62ce
Tongmean Teang <tongmean.teang@wdc.com>
2022-09-08 13:48:55 +0700

    2

---
+++ src/component/sideBar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className='container-fluid sidebar-section'>
+        <div className='sidebar-expand'>
+            <div className='icon-for-sidebar-expand-and-collapse'>
+                <p>
+                    
+                </p>
+
+            </div>
+        </div>
+
+        <div className='container-fluid'>
+            Home-section
+        </div>
+        
+    </div>
+  );
+}
+
+export default Sidebar
